170f356295c6adb81bcfa15fdd58d8dc8abc29a4
venturellim <mventu@libero.it>
2026-03-27 17:17:56 +0100

    Add files via upload

--- genres/metal/metalEngine.js
+++ genres/metal/metalEngine.js
@@ -19,7 +19,7 @@ import { initThemeEngine } from "./themeEngine.js";
 import { generateSongProgressions } from "./metalTheory.js";
 import { waitForInstruments } from "../../common.js";
 
-console.log("metalEngine.js ver. 016.2 loaded");
+console.log("metalEngine.js ver. 016.3 loaded");
 
 // ============================================================
 // 🎧 LOADER STRUMENTI METAL
@@ -473,7 +473,7 @@ drums.scheduleSection(sec, sec.scale, sec.progression, sec.riffResult.events);
         }, eventTime);
     });
 bass.scheduleTransition(sec, sec.transition);
-drums.scheduleTransition(sec, sec.transition.events);
+//drums.scheduleTransition(sec, sec.transition.events);
 
     // ---------------------------------------------------------
     // OPEN CHORD FINALE (solo per mixed e lead)
