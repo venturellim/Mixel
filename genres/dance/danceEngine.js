c57702c0d531f6ef6902b38f4a7072fc783b8f78
venturellim <mventu@libero.it>
2026-05-16 09:38:51 +0200

    Add files via upload

--- genres/dance/danceEngine.js
+++ genres/dance/danceEngine.js
@@ -19,7 +19,7 @@ export async function waitDanceInstruments() {
 export function createDanceEngine(params, score) {
     const rand = createSeededRandom(params.dna);
     
-    //params.global = { intensity: 0.3, mood: 0.5, complexity: 0.4 }; // → Gigi
+    params.global = { intensity: 0.3, mood: 0.5, complexity: 0.4 }; // → Gigi
     // params.global = { intensity: 0.7, mood: 0.5, complexity: 0.8 }; // → Eiffel65
     // params.global = { intensity: 0.7, mood: 0.7, complexity: 0.5 }; // → GabryPonte
     
