a9f0bb05f94b6356146463f84608c2fea60db407
venturellim <mventu@libero.it>
2026-05-21 22:27:31 +0200

    Add files via upload

--- genres/piano/pianoEngine.js
+++ genres/piano/pianoEngine.js
@@ -4,7 +4,13 @@ import { buildPianoParams } from "./pianoParams.js";
 import { buildSongStructure } from "../../utils/structureUtils.js";
 import { createSeededRandom } from "../../utils/randomUtils.js";
 import { generateSongProgressions, degreeToRoot } from "../../utils/musicTheory.js";
-import { piano, lhBus, rhBus, pianoInstruments, pianoVolumeMap } from "./pianoInstruments.js";
+import { 
+    pianoSalamander, 
+    lhBus, 
+    rhBus, 
+    pianoInstruments, 
+    pianoVolumeMap 
+} from "./pianoInstruments.js";
 import { schedulePianoRhythm } from "./pianoRhythmEngine.js";
 import { schedulePianoLead } from "./pianoLeadEngine.js";
 import { waitForInstruments } from "../../common.js";
@@ -83,9 +89,26 @@ export function createPianoEngine(params, score) {
         }, sec.startTime);
 
         // Tre motori paralleli come orchestra/metal
-        schedulePianoRhythm(sec, realNotes, { piano, lhBus, rhBus }, combinedParams, rand, measureDur, nextSectionRoot, score);
-        schedulePianoLead(sec, realNotes, { piano, lhBus, rhBus }, combinedParams, rand, measureDur, score);
-    });
+        schedulePianoRhythm(
+    sec, 
+    realNotes, 
+    { piano: pianoSalamander, lhBus, rhBus }, 
+    combinedParams, 
+    rand, 
+    measureDur, 
+    nextSectionRoot, 
+    score
+);
+
+schedulePianoLead(
+    sec, 
+    realNotes, 
+    { piano: pianoSalamander, lhBus, rhBus }, 
+    combinedParams, 
+    rand, 
+    measureDur, 
+    score
+);
 
     return {
         totalDuration: structure.totalDuration,
--- genres/piano/pianoInstruments.js
+++ genres/piano/pianoInstruments.js
@@ -58,4 +58,4 @@ export const pianoVolumeMap = {
     pianoRH: "Piano (Melodia/DX)"
 };
 
-export { normalizeNote };
+export { normalizeNote, pianoSalamander};
