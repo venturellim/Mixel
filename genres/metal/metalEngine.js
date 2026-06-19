73e67e60bf78d087256b6b4a0f0f8ccf017e6d5c
venturellim <mventu@libero.it>
2026-06-19 12:33:40 +0200

    Add files via upload

--- genres/metal/metalEngine.js
+++ genres/metal/metalEngine.js
@@ -8,13 +8,17 @@ import { metalVolumeMap } from "./metalInstruments.js";
 import { scheduleRhythm } from "./metalRhythmEngine.js";
 import { scheduleLead } from "./metalLeadEngine.js"; 
 import { initFxRack } from "../../utils/footswitchPreset.js";
+import { initFxController } from "../../utils/fxController.js";
 
 console.log("metalEngine.js ver. 020.2 loaded");
 
 export function createMetalEngine(params, score, instruments) {
 
-// Inizializza FX Rack all'avvio (non blocca)
+// Inizializza FX Rack
     initFxRack();
+    
+    // Inizializza il controller FX (UI)
+    initFxController();
 
     const rand = createSeededRandom(params.dna);
     const metalParams = buildPowerMetalParams(rand);
